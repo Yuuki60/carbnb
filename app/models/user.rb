b5b896726f1b708eab9c4db037d9a1573f4c4a74
Florent Vandroy <florentvandroy@gmail.com>
2024-03-12 15:05:18 +0100

    Add cars relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_many :cars
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :lockable, :confirmable, :timeoutable
 end

0fa3ec477462fa54599ef102767d2d8683002138
Daniel Guasco <daniele.scultura@libero.it>
2024-03-12 17:32:56 +0100

    désactivation des modules devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  has_many :cars
   # Include default devise modules. Others available are:
   # :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :lockable, :confirmable, :timeoutable
+         :recoverable, :rememberable, :validatable
   has_many :cars
 end

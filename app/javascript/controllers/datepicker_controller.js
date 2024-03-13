21df108fdccf8b5df59692994dce8527d6beaa70
Florent Vandroy <florentvandroy@gmail.com>
2024-03-13 12:36:31 +0100

    add flatpickr et datepicker controller

---
+++ app/javascript/controllers/datepicker_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from 'flatpickr';
+import rangePlugin from 'flatpickr/dist/plugins/rangePlugin';
+
+export default class extends Controller {
+  connect() {
+    flatpickr(this.element, {
+      minDate: 'today',
+      mode: "range",
+      inline: true,
+      allowInput: false,
+      "plugins": [new rangePlugin({ input: "#end_date"})]
+    })
+  }
+}

fd0e758d735efb4c02cb7757dc7632e7df323a6a
Florent Vandroy <florentvandroy@gmail.com>
2024-03-13 12:37:43 +0100

    Form with datepicker, remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :orders, onlu: [:index]
+  resources :orders, only: [:index]
   resources :cars, only: [:index, :new, :create, :show, :destroy] do
-    resources :orders, except: [:index]
+    resources :orders, only: [:create]
   end
 end

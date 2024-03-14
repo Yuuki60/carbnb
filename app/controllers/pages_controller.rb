f2756328b3abc0479d27c985a35241d0f78fe8fc
Florent Soler <florentdu13200@hotmail.fr>
2024-03-14 21:56:58 +0100

    home page custom et d'autres trucs !

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @cars = Car.search_by_name(params[:query])
+    @cars = Car.search_by_name(params[:query]).limit(3)
     respond_to do |format|
       format.html # Follow regular flow of Rails
       format.text { render partial: "partials/search_result", locals: { cars: @cars }, formats: [:html] }

4a4153124974955232f1a7904eb38e29d0fe927b
Florent Soler <florentdu13200@hotmail.fr>
2024-03-14 23:41:53 +0100

    change search custom

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,21 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @cars = Car.search_by_name(params[:query]).limit(3)
+    @cars = Car.search_by_name_and_location(params[:query])
+
+    if params[:startdate].present? && params[:enddate].present?
+      if params[:query] == ''
+        @cars = Car.all
+      end
+
+      start_date = Date.parse(params[:startdate])
+      end_date = Date.parse(params[:enddate])
+
+      @cars = @cars.where.not(id: Order.where("start_date <= ? AND end_date >= ?", end_date, start_date).select(:car_id))
+    end
+
+    @cars = @cars.limit(3)
+
     respond_to do |format|
       format.html # Follow regular flow of Rails
       format.text { render partial: "partials/search_result", locals: { cars: @cars }, formats: [:html] }

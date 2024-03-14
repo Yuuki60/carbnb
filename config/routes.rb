360973bf3c5c650643cdcaa9e5c569afc41fa2d1
Daniel Guasco <daniele.scultura@libero.it>
2024-03-14 12:36:29 +0100

    plusieurs champs dans descr

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :orders, only: [:index]
-  resources :cars, only: [:index, :new, :create, :show, :destroy] do
+  resources :cars do
     resources :orders, only: [:create]
   end
 end
